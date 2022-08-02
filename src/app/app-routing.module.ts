d69ca6577eb940587b04b9ea3b73de972b13c2f4
João Amaral <joao.amaral.almeida99@gmail.com>
2022-08-01 22:59:24 -0300

    feat: add route home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
 // Components
 import { SigninComponent } from './pages/signin/signin.component';
@@ -9,6 +10,7 @@ const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'login' },
   { path: 'login', component: SigninComponent },
   { path: 'signup', component: SignupComponent },
+  { path: 'home', component: HomeComponent },
 ];
 
 @NgModule({
