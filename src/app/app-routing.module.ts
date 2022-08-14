7c452f2182bd2c21d31a0ca0fbaf93c6e4a2425e
João Amaral <joao.amaral.almeida99@gmail.com>
2022-08-13 22:31:12 -0300

    feat: add new route to categories

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoriesComponent } from './pages/categories/categories.component';
 import { HomeComponent } from './pages/home/home.component';
 
 // Components
@@ -11,6 +12,7 @@ const routes: Routes = [
   { path: 'login', component: SigninComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'categories', component: CategoriesComponent },
 ];
 
 @NgModule({
