5cc7d248b39286a34dda04e63b7731369ecb3a9a
João Amaral <joao.amaral.almeida99@gmail.com>
2022-08-16 23:06:12 -0300

    feat: create form validators

--- src/app/pages/expenses/expenses.component.ts
+++ src/app/pages/expenses/expenses.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CategoriesService } from '../categories/categories.service';
 
 @Component({
   selector: 'app-expenses',
   templateUrl: './expenses.component.html',
-  styleUrls: ['./expenses.component.scss']
+  styleUrls: ['./expenses.component.scss'],
 })
 export class ExpensesComponent implements OnInit {
+  form: FormGroup = this.formBuilder.group({
+    value: [null, [Validators.required]],
+    date: ['', [Validators.required]],
+    category: ['', [Validators.required]],
+    description: [''],
+  });
 
-  constructor() { }
+  categories: any = [];
+
+  constructor(
+    private readonly formBuilder: FormBuilder,
+    private readonly categoriesService: CategoriesService
+  ) {}
 
   ngOnInit(): void {
+    this.getCategories();
   }
 
+  getCategories() {
+    this.categoriesService.getAllCategories().subscribe({
+      next: (response) => this.categories.push(response),
+      error: (error) => console.log(error),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
+  }
 }
